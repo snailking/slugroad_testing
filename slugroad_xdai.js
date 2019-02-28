15a0f3fe4a730d081997775e6eb957b6a5705511
SnailKing <39387494+snailking@users.noreply.github.com>
2019-02-28 18:55:18 +0100

    Add files via upload

--- slugroad_xdai.js
+++ slugroad_xdai.js
@@ -7,34 +7,25 @@ window.addEventListener('load', async () => {
     // Modern dapp browsers...
     if (window.ethereum) {
         window.web3 = new Web3(ethereum);
-		web3.version.getNetwork(function(error, result) {
-            if (!error) {
-                if (result == "100") {
-					try {
-						// Request account access if needed
-						await ethereum.enable();
-						// Acccounts now exposed
-						//web3.eth.sendTransaction({/* ... */});
-					} catch (error) {
-						// User denied account access...
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+			web3.version.getNetwork(function(error, result) {
+				if (!error) {
+					if (result != "100") {
+						console.log("Error: you must be on Ropsten Network to use this website.");
 					}
-                } else {
-                    console.log("Error: you must be on xDai Chain to use this website.");
-                }
-            }
-        });   
+				}
+			});
+            // Acccounts now exposed
+            //web3.eth.sendTransaction({/* ... */});
+        } catch (error) {
+            // User denied account access...
+        }
     }
     // Legacy dapp browsers...
     else if (window.web3) {
         window.web3 = new Web3(web3.currentProvider);
-		web3.version.getNetwork(function(error, result) {
-            if (!error) {
-                if (result == "100") {
-                } else {
-                    console.log("Error: you must be on xDai Chain to use this website.");
-                }
-            }
-        });
         // Acccounts always exposed
         //web3.eth.sendTransaction({/* ... */});
     }
